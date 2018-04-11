04388b4d835040ffdd4e69645784a725b365b77f
mshtang <32868278+mshtang@users.noreply.github.com>
2018-04-11 11:28:44 +0200

    initial commit

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+bool isPalindrome(int x);
+
+int main()
+{
+    int a, b, c;
+    std::cout << "a=12345 is " << isPalindrome(12345);
+    std::cout << "\na=12321 is " << isPalindrome(12321);
+    std::cout << "\na=123321 is " << isPalindrome(123321);
+}
+
+bool isPalindrome(int x)
+{
+    if (x < 0 || x % 10 == 0)
+        return false;
+    int res = 0;
+    while (x > res)
+    {
+        res = res * 10 + x % 10;
+        x = x / 10;
+    }
+    return ((x == res) || (x == res / 10));
+}
